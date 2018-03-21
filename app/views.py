b4fa7437039a2762f6550198385f95cb56c916aa
lujianhua-ops <lbh0417@163.com>
2018-03-21 16:31:26 +0800

    add user login

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
 # -*-coding:utf8-*-
 # 
-from flask import render_template,flash,redirect
-from app import app
+from flask import render_template,flash,redirect,session,url_for,request,g
+from flask.ext.login import login_user,logout_user,current_user,login_required
+from app import app,db,lm,oid
 from .forms import LoginForm
+from .models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = { 'nickname':'Miguel'}
+	user = g.user
 	posts = [
 		{
 			'author':{'nickname':'John'},
@@ -25,7 +28,10 @@ def index():
 		posts = posts)
 
 @app.route('/login',methods = ['GET','POST'])
+@oid.loginhandler
 def login():
+	if g.user is not None and g.user.is_authenticated:
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash('Login requested for OpenID="' + form.openid.data + '",rememver_me=' + str(form.remember_me.data))
@@ -34,3 +40,51 @@ def login():
 		title = 'Sign In',
 		form = form,
 		providers = app.config['OPENID_PROVIDERS'])
+
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+@oid.after_login
+def after_login(resp):
+	if resp.email is None or resp.email == "":
+		flash('Invalid login.Please try again.')
+		return redirect(url_for('login'))
+	user = User.query.filter_by(email=resp.email).first()
+	if user is None:
+		nickname = resp.nickname
+		if nickname is None or nickname == "":
+			nickname = resp.email.split('@')[0]
+		user = User(nickname=nickname,email=resp.email)
+		db.session.add(user)
+		db.session.commit()
+	remember_me = False
+	if 'remember_me' in session:
+		remember_me = session['remember_me']
+		session.pop('remember_me',None)
+	login_user(user,remember = remember_me)
+	return redirect(request.args.get('next') or url_for('index'))
+
+@app.before_request
+def before_request():
+	g.user = current_user
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+	user = User.query.filter_by('nickname = nickname').first()
+	if user == None:
+		flash('User ' + nickname + ' not found.')
+		return redirect(url_for('index'))
+	posts = [
+		{'author': user,'body':'Test post #1'},
+		{'author': user,'body':'Test post #2'}
+	]
+	return render_template('user.html',
+		user = user,
+		posts = posts)
